f89ff8cdae49f1442f6a95e399c813f34626cde5
dl4184 <dl4184@student.uni-lj.si>
2016-07-12 16:40:32 +0200

    prvi dan 2-del

--- DrJames/app/src/main/java/marand/drjames/MainActivity.java
+++ DrJames/app/src/main/java/marand/drjames/MainActivity.java
@@ -1,7 +1,9 @@
 package marand.drjames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void openTestActivity(View view){
+        Intent intent = new Intent(this, TestActivity.class);
+        startActivity(intent);
     }
 }
