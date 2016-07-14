1bc95c0ff1417e741ebb908a78720b3b69539da4
dl4184 <dl4184@student.uni-lj.si>
2016-07-14 11:57:37 +0200

    Izboljsava izgleda prototipa

--- DrJames/app/src/main/java/marand/drjames/AboutActivity.java
+++ DrJames/app/src/main/java/marand/drjames/AboutActivity.java
@@ -4,11 +4,12 @@ package marand.drjames;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class TestActivity extends AppCompatActivity{
+public class AboutActivity extends AppCompatActivity{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_about);
 
     }
 }
--- DrJames/app/src/main/java/marand/drjames/MainActivity.java
+++ DrJames/app/src/main/java/marand/drjames/MainActivity.java
@@ -1,9 +1,11 @@
 package marand.drjames;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void openTestActivity(View view){
-        Intent intent = new Intent(this, TestActivity.class);
+    public void openAboutActivity(View view){
+        Intent intent = new Intent(this, AboutActivity.class);
         startActivity(intent);
     }
+
+    public void buttonClick(View view){
+        TextView t =(TextView) findViewById(R.id.instructions_start);
+        t.setText(R.string.instruction_start2);
+        TextView t2 =(TextView) findViewById(R.id.instructions);
+        t2.setText(R.string.instructions2);
+        Button b=(Button) findViewById(R.id.button);
+        b.animate();
+    }
+
 }
